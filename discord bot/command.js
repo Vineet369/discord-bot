3b0349668f6355adb7dcb328638730502c00642a
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2024-03-26 15:01:13 +0530

    Update command.js

--- discord bot/command.js
+++ discord bot/command.js
@@ -7,12 +7,12 @@ const commands = [
     },
   ];
 
-const rest = new REST({ version: '10' }).setToken("MTIyMjA0NzM0NzY0MzUxNDkxMg.GUGjP_.d6XODpe3EN1aUpgwg5S5Tv04e_Mz2hXFqgWJlY");
+const rest = new REST({ version: '10' }).setToken("token");
 
 try {
     console.log('Started refreshing application (/) commands.');
   
-    await rest.put(Routes.applicationCommands("1222047347643514912"), { body: commands });
+    await rest.put(Routes.applicationCommands("userid"), { body: commands });
   
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
